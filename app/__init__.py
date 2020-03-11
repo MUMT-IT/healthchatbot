49ec14d3c3fb44f0372be4355b5b891c7834c75c
Likit Preeyanon <likit.pre@mahidol.edu>
2020-03-11 23:09:10 +0700

    removed FLASK_ENV setting

--- app/__init__.py
+++ app/__init__.py
@@ -8,5 +8,4 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-    app.config['FLASK_ENV'] = os.environ.get('FLASK_ENV')
     return app
