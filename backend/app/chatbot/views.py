136c0558244d362f4347b49c83f09544785c39ff
Likit Preeyanon <likit.pre@mahidol.edu>
2020-12-13 08:04:59 +0700

    updated/added more functions to respond to new actions

--- backend/app/chatbot/views.py
+++ backend/app/chatbot/views.py
@@ -6,7 +6,7 @@ from linebot.exceptions import InvalidSignatureError
 from wsgi import app
 from rdflib_sqlalchemy import registerplugins
 from rdflib.store import Store
-from rdflib import URIRef, Graph, plugin, Namespace, FOAF, RDFS
+from rdflib import URIRef, Graph, plugin, Namespace, FOAF, RDFS, Literal
 from rdflib.plugins.sparql import prepareQuery
 from app import DB_URI
 
@@ -21,8 +21,20 @@ graph = Graph(store, identifier=identifier)
 graph.open(DB_URI)
 
 labn = Namespace('http://mtclan.net/rdfs/labtests/')
+ns2 = Namespace('http://mtclan.net/rdfs/riskfactors/')
 
 FEMALE_ENDING = 'ค่ะ'
+
+def join_and(alist, sep=',', conj='และ'):
+    text = ''
+    for i in range(len(alist)):
+        text += alist[i]
+        if i != len(alist) - 2:
+            if i != len(alist) - 1:
+                text += sep
+        else:
+            text += conj
+    return text
 
 def insert_and(items):
     if len(items) > 1:
@@ -51,11 +63,10 @@ def get_tests_list():
 
 @bot.route('/tests/info/<testname>')
 def test_info(testname):
-    q = prepareQuery(
-        ('SELECT ?atest ?analname ?fastinfo WHERE '
-        '{ ?atest lab:tests ?analyte .'
-         '?analyte foaf:name ?analname .'
-         '?atest lab:fasting ?fastinfo .}'),
+    q = prepareQuery(('SELECT ?atest ?analname ?fastinfo WHERE {'
+                      '?analyte foaf:name ?analname .'
+                      '?atest lab:tests ?analyte .'
+                      '?atest lab:fasting ?fastinfo .}'),
         initNs={'foaf': FOAF, 'lab': labn}
     )
     test = labn[testname.upper()]
@@ -100,100 +111,71 @@ def line_message_callback():
     return 'OK'
 
 
+def get_self_prep_from_test_code(req):
+    tcode = req.get('queryResult').get('parameters').get('tests')
+    if not tcode:
+        return None
+    q = prepareQuery(
+        ('SELECT ?atest ?tcode ?fastinfo WHERE '
+         '{?atest lab:code ?tcode .'
+         '?atest lab:fasting ?fastinfo .'
+         '}'),
+        initNs={'lab': labn}
+    )
+    message = 'การเตรียมตัวเพื่อการตรวจควรปฏิบัติดังนี้'
+    for row in graph.query(q, initBindings={'tcode': tcode}):
+        message += ' ' + row.fastinfo
+    return message
+
+
 def get_test_info_from_code(req):
     tcode = req.get('queryResult').get('parameters').get('tests')
     if not tcode:
         return None
     tests = []
     q = prepareQuery(
-        ('SELECT ?atest ?testname ?tcode ?analname ?fastinfo WHERE '
-         '{?atest lab:code ?tcode .'
-         '?atest foaf:name ?testname .'
+        ('SELECT ?atest ?testname ?tcode ?analname ?fastinfo WHERE {'
+         '?analyte rdfs:label ?analname .'
          '?atest lab:tests ?analyte .'
-         '?analyte foaf:name ?analname .'
-         '?atest lab:fasting ?fastinfo .}'),
+         '?atest lab:code ?tcode .'
+         '?atest foaf:name ?testname .'
+         '?atest lab:fasting ?fastinfo .'
+         'FILTER (lang(?analname) = "th")'
+         '}'),
         initNs={'foaf': FOAF, 'lab': labn}
     )
     data = []
     message = ''
     for row in graph.query(q, initBindings={'tcode': tcode}):
-        message += '{} ({}) เป็นการตรวจหา {}'.format(row.testname, row.tcode, row.analname)
-    '''
-    q = prepareQuery(
-        'SELECT ?specimens ?label WHERE { ?atest lab:testWith ?specimens . ?specimens rdfs:label ?label}',
-        initNs={'lab': labn}
-    )
-    specimens = []
-    for row in graph.query(q, initBindings={'test': data[0]['test']}):
-        specimens.append({
-            'specimens': row.specimens,
-            'label': row.label
+        data.append({
+            'test': row.atest,
+            'testname': row.testname,
+            'tcode': row.tcode,
+            'analname': row.analname
         })
-    data[0]['specimens'] = specimens
-    '''
+
+    for t in data:
+        q = prepareQuery(
+            'SELECT ?specimens ?label WHERE {'
+            '?atest lab:code ?tcode .'
+            '?atest lab:testWith ?specimens .'
+            '?specimens rdfs:label ?label}',
+            initNs={'lab': labn}
+        )
+        specimens = []
+        for row in graph.query(q, initBindings={'tcode': tcode}):
+            specimens.append(row.label)
+        t['specimens'] = specimens
+    for t in data:
+        message += '{} ({}) เป็นการตรวจ{} ใน{}'\
+            .format(t['testname'], t['tcode'], t['analname'], ' '.join(t['specimens']))
+    message += ' ต้องการทราบรายละเอียดเกี่ยวกับเตรียมตัวก่อนการตรวจไหมคะ'
     return message
-
-def get_tests_for_risk(req):
-    diseases = req.get('queryResult').get('parameters').get('diseases', '')
-    specimens = req.get('queryResult').get('parameters').get('specimens', '')
-    message = ''
-    submessages = []
-    for ds in graph.nodes.match('Disease', thname=diseases):
-        for rel in graph.match((ds, ), r_type='SCREENED_BY'):
-            submessage = ''
-            test = rel.end_node
-            print(test)
-            if test['testgroup']:
-                submessage += ' กลุ่ม {} ได้แก่ '.format(test['thname'])
-                for rel in graph.match((test,), r_type='INCLUDES'):
-                    submessage += '{} '.format(rel.end_node['thname'])
-            else:
-                submessage += '{} '.format(test['thname'])
-            submessages.append(submessage)
-    if submessages:
-        submessages = ['การตรวจความเสี่ยง{} มีรายการตรวจ'.format(diseases)] + submessages
-        message = ' '.join(submessages)
-    return message
-
-
-def get_risk_factors_of_diseases(req):
-    parameter = req.get('queryResult').get('parameters').get('parameter', '')
-    diseases = req.get('queryResult').get('parameters').get('diseases', '')
-    age = req.get('queryResult').get('parameters').get('number-integer', 0)
-    message = ''
-    if diseases:
-        factors = []
-        for ds in graph.nodes.match('Disease', thname=diseases):
-            for rel in graph.match((ds, ), r_type='ASSOCIATED_BY'):
-                factors.append(rel.end_node['thname'])
-        if factors:
-            message = 'ปัจจัยเสี่ยงต่อ{} ได้แก่ {}'.format(diseases, ' '.join(insert_and(factors)))
-        return message
-    elif parameter:
-        diseases = []
-        for par in graph.nodes.match('Parameter', thname=parameter):
-            for rel in graph.match((par, ), r_type='ASSOCIATES_WITH'):
-                diseases.append(rel.end_node['thname'])
-        if diseases:
-            message = '{} เกี่ยวข้องกับ {}'.format(parameter, ' '.join(insert_and(diseases)))
-        return message
-    if age:
-        diseases = []
-        agenode = graph.nodes.match('Parameter', enname='age').first()
-        if age > 65:
-            for rel in graph.match((agenode, ), r_type='ASSOCIATES_WITH'):
-                if rel['greater'] <= age:
-                    diseases.append(rel.end_node['thname'])
-        if diseases:
-            message = 'อายุ {} เกี่ยวข้องกับ {}'.format(age, ' '.join(insert_and(diseases)))
-        return message
 
 
 def get_tests_for_concern(req):
-    parameter = req.get('queryResult').get('parameters').get('parameter', '')
     diseases = req.get('queryResult').get('parameters').get('diseases', '')
     print('disease is {}'.format(diseases))
-    age = req.get('queryResult').get('parameters').get('number-integer', 0)
     if not diseases:
         return None
     tests = []
@@ -201,16 +183,193 @@ def get_tests_for_concern(req):
         ('SELECT ?testname ?tcode WHERE { ?a rdfs:label ?disease .'
          '?test lab:screenFor ?a .'
          '?test lab:code ?tcode .'
-         '?test foaf:name ?testname .}')
+         '?test rdfs:label ?testname .}')
         , initNs={'lab': labn, 'foaf': FOAF, 'rdfs': RDFS}
     )
     for row in graph.query(q, initBindings={'disease': diseases}):
+        print(row.testname)
         tests.append({
             'name': row.testname + " ({})".format(row.tcode)
         })
     instruction = 'กรุณาพิมพ์รหัสในวงเล็บสำหรับรายละเอียดของรายการตรวจ'
-    message = 'ควรตรวจ {}\n{}'.format(' '.join([t['name'] for t in tests]), instruction)
+    message = 'ควรตรวจ {} {}'.format(join_and([t['name'] for t in tests], sep=" "), instruction)
     return message
+
+
+def get_risk_disease_by_age(req):
+    age = req.get('queryResult').get('parameters').get('age')
+    if age is None:
+        return None
+    print(age)
+    q = prepareQuery(('SELECT ?disease ?name WHERE { '
+                      '?disease rdfs:label ?name .'
+                      '?disease lab:startRiskAge ?age .'
+                      'FILTER (?a >= ?age)'
+                      '}'),
+                     initNs={'lab': labn, 'rdfs': RDFS})
+    message = 'ในช่วงวัยของคุณอาจมีความเสี่ยงต่อ'
+    found = False
+    for row in graph.query(q, initBindings={'a': Literal(int(age))}):
+        message += '{} '.format(row.name)
+        found = True
+    if not found:
+        message = 'ไม่พบโรคที่มีความเสี่ยงในช่วงวัยของคุณ ทั้งนี้กรุณาปรึกษานักเทคนิคการแพทย์หรือสอบถามเราเกี่ยวกับความเสี่ยงของโรคที่เกิดจากปัจจัยอื่น'
+    return message
+
+
+def get_self_preparation(req):
+    checkup = req.get('queryResult').get('parameters').get('checkups')
+    params = req.get('queryResult').get('parameters').get('parameter')
+    analyte = req.get('queryResult').get('parameters').get('analytes')
+    interfs = set()
+    if checkup and params:
+        tests = set()
+        q = prepareQuery((
+            'SELECT ?testname ?interferance WHERE {'
+            '?checkup rdfs:label ?label .'
+            '?checkup lab:requires ?specimens .'
+            '?test lab:testWith ?specimens .'
+            '?test rdfs:label ?testname .'
+            '?test lab:interferedBy ?interferance .'
+            '?interferance rdfs:label ?param'
+            '}'
+        ), initNs={'lab': labn, 'rdfs': RDFS})
+
+        for param in params:
+            for row in graph.query(q, initBindings={'label': checkup, 'param': param}):
+                tests.add(row.testname)
+                interfs.add(param)
+        if tests:
+            message = '{}อาจรบกวนผลการตรวจได้แก่'.format(join_and(list(interfs), sep=' '))
+            return message + join_and(list(tests), sep=' ')
+        else:
+            return 'ไม่พบรายงานว่า{}รบกวนการตรวจ'.format(join_and(params, sep=' '))
+    return None
+
+
+def get_disease_from_risk_factor(req):
+    factor = req.get('queryResult').get('parameters').get('risk-factor')
+    q = prepareQuery((
+        'SELECT ?disname WHERE {'
+        '?factor rdfs:label ?factorname .'
+        '?factor ns2:causes ?disease .'
+        '?disease rdfs:label ?disname .'
+        'FILTER (LANG(?disname) = "th")'
+        '}'
+    ), initNs={'rdfs': RDFS, 'ns2': ns2})
+    message = 'คุณมีความเสี่ยงต่อโรค'
+    diseases = []
+    for row in graph.query(q, initBindings={'?factorname': factor}):
+        diseases.append(row.disname)
+    message += join_and(diseases, sep=' ')
+    return message
+
+
+def get_reference_value(req):
+    analyte = req.get('queryResult').get('parameters').get('analytes')
+    specimens = req.get('queryResult').get('parameters').get('specimens')
+    q = prepareQuery((
+        'SELECT ?hival ?lowval ?tname ?unit WHERE {'
+        '?specimens rdfs:label ?slabel .'
+        '?test lab:testWith ?specimens .'
+        '?analyte rdfs:label ?alabel .'
+        '?test lab:tests ?analyte .'
+        '?test rdfs:label ?tname .'
+        '?test lab:normalHi ?hival .'
+        '?test lab:normalLow ?lowval .'
+        '?test lab:unit ?unit .'
+        '}'), initNs={'lab': labn, 'rdfs': RDFS})
+    tests = []
+    for row in graph.query(q, initBindings={'slabel': specimens, 'alabel': analyte}):
+        tests.append({
+            'name': row.tname,
+            'hi': row.hival,
+            'low': row.lowval,
+            'unit': row.unit
+        })
+
+    if not tests:
+        return None
+
+    message = 'ค่าปกติของการตรวจหา{}ใน{}จากการตรวจ'.format(analyte, specimens)
+    for t in tests:
+        message += '{}คือ {} - {} {} '.format(t['name'], t['low'], t['hi'], t['unit'])
+    return message
+
+
+def get_lab_result_interpretation(req):
+    value = req.get('queryResult').get('parameters').get('number')
+    disease = req.get('queryResult').get('parameters').get('diseases')
+    analyte = req.get('queryResult').get('parameters').get('analytes')
+    specimens = req.get('queryResult').get('parameters').get('specimens')
+    if value and disease and analyte and specimens:
+        q = prepareQuery((
+            'SELECT ?ctvalue ?ctside ?disname WHERE {'
+            '?analyte rdfs:label ?analabel .'
+            '?specimens rdfs:label ?splabel .'
+            '?test lab:tests ?analyte .'
+            '?test lab:testWith ?specimens .'
+            '?ct lab:atest ?test .'
+            '?ct lab:cutoff ?ctvalue .'
+            '?ct lab:side ?ctside .'
+            '?condition lab:indicatedBy ?ct .'
+            '?condition ns2:causes ?disease .'
+            '?disease rdfs:label ?disname .'
+            '}'
+        ), initNs={'ns2': ns2, 'lab': labn, 'rdfs': RDFS})
+
+        diseases = set()
+        for row in graph.query(q, initBindings={'analabel': analyte,
+                                                'splabel': specimens,
+                                                'disname': disease}):
+            if row.ctside == Literal('right'):
+                if Literal(value) >= row.ctvalue:
+                    diseases.add(row.disname)
+            elif row.ctside == Literal('left'):
+                if Literal(value) <= row.ctvalue:
+                    diseases.add(row.disname)
+
+        if not diseases:
+            return 'ไม่มีความเสี่ยง'
+        return 'คุณมีความเสี่ยงเป็น' + disease
+
+    if value and analyte and specimens:
+        q = prepareQuery((
+            'SELECT ?ctvalue ?ctside ?disname ?hi ?low ?unit WHERE {'
+            '?analyte rdfs:label ?analabel .'
+            '?test lab:tests ?analyte .'
+            '?specimens rdfs:label ?splabel .'
+            '?test lab:testWith ?specimens .'
+            '?test lab:normalHi ?hi .'
+            '?test lab:normalLow ?low .'
+            '?test lab:unit ?unit .'
+            '?ct lab:atest ?test .'
+            '?ct lab:cutoff ?ctvalue .'
+            '?ct lab:side ?ctside .'
+            '?condition lab:indicatedBy ?ct .'
+            '?condition ns2:causes ?disease .'
+            '?disease rdfs:label ?disname .'
+            '}'
+        ), initNs={'ns2': ns2, 'lab': labn, 'rdfs': RDFS})
+
+        message = ''
+        diseases = set()
+        for row in graph.query(q, initBindings={'analabel': analyte, 'splabel': specimens}):
+            if row.ctside == Literal('right'):
+                if Literal(value) >= row.ctvalue:
+                    if row.low == Literal(0) or row.low == Literal(0.0):
+                        row.low = 'น้อยกว่า '
+                    else:
+                        row.low = row.low + '-'
+                    diseases.add((row.disname, row.hi, row.low, row.unit))
+            elif row.ctside == Literal('left'):
+                if Literal(value) <= row.ctvalue:
+                    diseases.add((row.disname, row.hi, row.low, row.unit))
+
+        message += join_and(['{} (ค่าปกติคือ {}{} {})'.format(d[0], d[2], d[1], d[3]) for d in diseases], sep=' ')
+        if not message:
+            return 'ไม่มีความเสี่ยง'
+        return 'คุณมีความเสี่ยงเป็น' + message + ' ทั้งนี้ควรปรึกษานักเทคนิคการแพทย์เพิ่มเติมเพื่อตรวจยืนยันผล'
 
 
 @bot.route('/dialogflow/webhook', methods=['POST'])
@@ -223,25 +382,22 @@ def dialogflow_webhook():
     print(action)
     if action == 'get_test_info':
         message = get_test_info_from_code(req)
+    if action == 'get_self_prep_from_test':
+        message = get_self_prep_from_test_code(req)
     if action == 'get_tests_for_concern':
         message = get_tests_for_concern(req)
-    if action == 'get_tests_for_risk':
-        message = get_tests_for_risk(req)
-    if action == 'get_risk_factors_of_diseases':
-        message = get_risk_factors_of_diseases(req)
-    if action == 'get_min_fasting_time':
-        analyte = req.get('queryResult').get('parameters').get('testname', '')
-        specimens = req.get('queryResult').get('parameters').get('specimens', '')
-        print('{}, {}'.format(analyte, specimens))
-        for n in graph.nodes.match('Analyte', thname=analyte):
-            if n['testgroup']:
-                message += 'รายการตรวจในกลุ่ม {} ได้แก่ '.format(n['thname'])
-                for rel in graph.match((n,), r_type='INCLUDES'):
-                    message += '{} ควรงดอาหาร {} '.format(rel.end_node['thname'],
-                                              rel.end_node['fast'])
-                message = message.rstrip(',')
-            else:
-                message += 'ควรงดอาหารประมาณ {}'.format(n['fast'])
+    if action == 'get_risk_disease_by_age':
+        message = get_risk_disease_by_age(req)
+    if action == 'self-preparation':
+        message = get_self_preparation(req)
+    if action == 'get_disease_from_risk_factor':
+        message = get_disease_from_risk_factor(req)
+    if action == 'get_reference_value':
+        message = get_reference_value(req)
+    if action == 'get_lab_result_interpretation':
+        message = get_lab_result_interpretation(req)
     if message:
-        rsp_message['fulfillmentText'] = message + ' ' + FEMALE_ENDING
+        if not message.endswith('คะ'):
+            message += FEMALE_ENDING
+        rsp_message['fulfillmentText'] = message
     return make_response(jsonify(rsp_message))
