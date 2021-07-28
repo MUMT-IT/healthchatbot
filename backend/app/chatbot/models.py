472afa5a9401558ccc97b6050e26583cd7b57cb2
Likit Preeyanon <likit.pre@mahidol.edu>
2021-07-28 21:40:19 +0700

    added line id field

--- backend/app/chatbot/models.py
+++ backend/app/chatbot/models.py
@@ -1,8 +1,8 @@
 from app import db
 
-
 class UnfulfilledMessage(db.Model):
     __tablename__ = 'unfulfilled_messages'
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String(255), nullable=False)
+    line_id = db.Column(db.String(255))
     created_at = db.Column(db.DateTime(timezone=True))
